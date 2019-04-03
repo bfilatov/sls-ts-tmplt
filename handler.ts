4cfa12bf6113ede9d8e65e2613eda0aa07d98f59
Valentin Zlydnev <jettary@gmail.com>
2019-04-03 12:07:46 +0300

    Some improvements on deploy process, adjust IAM user access

--- handler.ts
+++ handler.ts
@@ -5,7 +5,7 @@ export const hello: APIGatewayProxyHandler = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+      message: 'Go Serverless Webpack (Typescript) v1.0.5! Your function executed successfully!',
       input: event,
     }),
   };
